238bc4faccf0a0033395027bd85f04fc13c0a4dd
Jelle07 <113422379+JManders07@users.noreply.github.com>
2022-11-15 12:58:35 +0100

    Create architecture

---
+++ BeerWithFriends-BackEnd/BeerWithFriends-BackEnd/Controllers/BeerController.cs
@@ -0,0 +1,29 @@
+﻿using BeerWithFriendsCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeerWithFriends_BackEnd.Controllers
+{
+    [Route("api/[controller][action]")]
+    [ApiController]
+    public class BeerController : Controller
+    {
+        private readonly BeerLogic _beerLogic;
+
+        public BeerController(BeerLogic beerLogic)
+        {
+            _beerLogic = beerLogic;
+        }
+
+        [HttpGet]
+        public JsonResult Beers()
+        {
+            List<Beer> beers = new();
+            foreach (var beer in _beerLogic.Beers())
+            {
+                beers.Add(beer);
+            }
+            return Json(beers);
+        }
+    }
+}
---
+++ BeerWithFriends-BackEnd/BeerWithFriends-BackEnd/Program.cs
@@ -0,0 +1,42 @@
+using BeerWithFriendsContract;
+using BeerWithFriendsDAL;
+using BeerWithFriendsCore;
+using BeerWithFriendsDAL.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors();
+builder.Services.AddTransient<BeerDAL>();
+builder.Services.AddTransient<BeerLogic>();
+builder.Services.AddScoped<IBeerDAL, BeerDAL>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseCors(options =>
+{
+    options.AllowAnyHeader();
+    options.AllowAnyMethod();
+    options.AllowAnyOrigin();
+});
+
+app.MapControllers();
+
+app.Run();
